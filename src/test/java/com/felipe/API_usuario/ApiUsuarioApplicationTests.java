7e37268b875491f726b52266c78a778130d9d19f
felipeangeli <felipe.angeli@gmail.com>
2025-02-24 15:23:26 -0300

    commit inicial

---
+++ src/test/java/com/felipe/API_usuario/ApiUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.API_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
