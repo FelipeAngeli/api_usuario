691461d50a0a4cd721d50e6255c65accfe94110f
felipeangeli <felipe.angeli@gmail.com>
2025-02-26 10:03:49 -0300

    inclusao de metodo put de usuario

--- src/main/java/com/felipe/API_usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/felipe/API_usuario/business/converter/UsuarioConverter.java
@@ -125,4 +125,16 @@ public class UsuarioConverter {
                 .ddd(telefone.getDdd())
                 .build();
     }
+
+    public  Usuario updateUsuario(UsuarioDTO usuarioDTO, Usuario entity){
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome() != null ? usuarioDTO.getNome() : entity.getNome())
+                .id(entity.getId())
+                .senha(usuarioDTO.getSenha() != null ? usuarioDTO.getSenha() : entity.getSenha())
+                .email(usuarioDTO.getEmail() != null ? usuarioDTO.getEmail() : entity.getEmail())
+                .endereco(entity.getEndereco())
+                .telefones(entity.getTelefones())
+                .build();
+    }
+
 }
