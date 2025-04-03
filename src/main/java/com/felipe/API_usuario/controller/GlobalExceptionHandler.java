617da95456b1bae0d386017fe6f70e4a7e29b913
felipeangeli <felipe.angeli@gmail.com>
2025-04-03 18:06:57 -0300

    add endpoint para busca de cep

--- src/main/java/com/felipe/API_usuario/controller/GlobalExceptionHandler.java
+++ src/main/java/com/felipe/API_usuario/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.felipe.API_usuario.controller;
 
 import com.felipe.API_usuario.infrastructure.exceptions.ConflitExceptions;
+import com.felipe.API_usuario.infrastructure.exceptions.IllegalArgumentException;
 import com.felipe.API_usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.felipe.API_usuario.infrastructure.exceptions.UnauthorizedException;
 import org.springframework.http.HttpStatus;
@@ -25,4 +26,10 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> unauthorizedException(UnauthorizedException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> illegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
 }
